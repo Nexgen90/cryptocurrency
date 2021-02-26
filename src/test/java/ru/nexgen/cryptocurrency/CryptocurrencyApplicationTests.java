a9f605b653c76a03eeac8cebfc28de27f7ca66fe
Nikolay Mikutskiy <nskmik@mail.ru>
2021-02-26 18:33:38 +0300

    add blank spring boot project

---
+++ src/test/java/ru/nexgen/cryptocurrency/CryptocurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nexgen.cryptocurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
